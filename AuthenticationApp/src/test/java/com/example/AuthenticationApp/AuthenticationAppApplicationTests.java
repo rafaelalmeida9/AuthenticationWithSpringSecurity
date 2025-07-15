4f951deb8015ae1b24cd3bfcc4a2273d9f1f8c5b
rafaelalmeida9 <rafaeldealmeida@usp.br>
2025-07-15 20:21:28 -0300

    first commit

---
+++ AuthenticationApp/src/test/java/com/example/AuthenticationApp/AuthenticationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AuthenticationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
